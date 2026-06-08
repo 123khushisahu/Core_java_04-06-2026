c5bc3d8307125337960d38fc73e225ae30891b1e
123khushisahu <khushisahu9580@gmail.com>
2026-06-08 15:22:42 +0530

    done

--- Core_Java_Practice_Session/src/main/java/com/corejava/Fundamentals/Demo.java
+++ Core_Java_Practice_Session/src/main/java/com/corejava/Fundamentals/Demo.java
@@ -1,9 +1,18 @@
 package com.corejava.Fundamentals;
 
-public class Demo {
+import com.core_java.Oop.Encapsulation_example;
+
+public class Demo  {
 
 	public static void main(String[] args) {
 		System.out.println("hi this is the core java demo session------");
+		
+		Encapsulation_example en=new Encapsulation_example();
+		
+		
+		
+		
+		
 
 	}
 
