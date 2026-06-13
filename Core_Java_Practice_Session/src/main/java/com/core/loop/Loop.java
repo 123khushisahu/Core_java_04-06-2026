f2dfe4a0f3d65f35b499c4f2a9cdd2347e943ab7
123khushisahu <khushisahu9580@gmail.com>
2026-06-13 22:17:49 +0530

    done

---
+++ Core_Java_Practice_Session/src/main/java/com/core/loop/Loop.java
@@ -0,0 +1,39 @@
+package com.core.loop;
+
+public class Loop {
+
+	public static void main(String[] args) {//args vari
+		
+		
+		/*Interview Answer
+		How many types of loops are there in Java?
+
+		Java has 4 types of loops:
+
+		for loop
+		while loop
+		do-while loop
+		 for-each loop
+		*/
+		
+//		for(int i=10;i>=0;i--)	{
+//			System.out.println(i);
+//		}
+//		
+		for (int i = 1;i<=20; i++) {
+		    if (i % 2 == 0) {
+		        System.out.println(i);
+		    }
+//		    for(int j=1;j<=50;j++) {
+//		    	if(j%3==0) {
+//		    		System.out.println(j);
+//		    	}
+		    }
+		
+		}
+			
+		}
+
+	
+
+
---
+++ Core_Java_Practice_Session/src/main/java/com/core/operator/Operator_all_examples.java
@@ -0,0 +1,56 @@
+package com.core.operator;
+
+public class Operator_all_examples {
+
+	public static void main(String[] args) {
+		/*
+		| S.No | Operator Type        | Examples                         |             |        |
+		| ---- | -------------------- | -------------------------------- | ----------- | ------ |
+		| 1    | Arithmetic Operators | `+`, `-`, `*`, `/`, `%`          |             |        |
+		| 2    | Unary Operators      | `++`, `--`, `!`, `+`, `-`        |             |        |
+		| 3    | Assignment Operators | `=`, `+=`, `-=`, `*=`, `/=`      |             |        |
+		| 4    | Relational Operators | `==`, `!=`, `>`, `<`, `>=`, `<=` |             |        |
+		| 5    | Logical Operators    | `&&`, `                          |             | `, `!` |
+		| 6    | Bitwise Operators    | `&`, `                           | `, `^`, `~` |        |
+		| 7    | Shift Operators      | `<<`, `>>`, `>>>`                |             |        |
+		| 8    | Ternary Operator     | `?:`                             |             |        |
+
+		*/
+		
+		
+		
+		
+
+       // 1.Arithmatic operator
+		int m1=200;
+		int m2=100;
+		
+		System.out.println(m1+m2);
+		System.out.println(m1-m2);
+		System.out.println(m1*m2);
+		System.out.println(m1/m2);
+		System.out.println(m1%m2);
+		
+		
+		//2. Unary Operators
+
+		//Work on a single operand.
+
+		//Increment
+		
+		int a=5;
+		
+		
+
+
+		
+		a++;
+		System.out.println(a);
+		
+		
+		
+		
+
+	}
+
+}
